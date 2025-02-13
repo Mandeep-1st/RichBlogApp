f731ed39ee49b0b2a82a5bd2c2f5c11bc0404b3c
Mandeep Sharma <mandeepsharma0701@gmail.com>
2025-02-13 13:07:06 +0530

    Deployment

---
+++ src/components/Logo.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Logo({ width = "100px" }) {
+  return (
+    <div className="h-12 w-12">
+      <img
+        src="https://img.freepik.com/premium-vector/black-white-picture-website-black-background_1186366-88850.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Logo;
